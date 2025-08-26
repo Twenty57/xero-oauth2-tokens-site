bed5e79de9fb32e46cb1e8c7318dfb87c967ccdb
Gawie Yssel <gawie.yssel@twenty57.com>
2025-08-26 15:03:28 +0200

    migrate to Vite

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+    react({
+      include: "**/*.{jsx,tsx,js,ts}",
+    })
+  ],
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'build'
+  }
+})
